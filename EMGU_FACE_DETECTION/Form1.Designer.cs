5c94411494bb39e75de88504dcf13920d21a58ad
Jaskirat Singh Ubhi <jaskiratubhi2@gmail.com>
2019-11-25 10:10:34 -0800

    Initial commit

---
+++ EMGU_FACE_DETECTION/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace EMGU_FACE_DETECTION
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.camDevice = new System.Windows.Forms.ComboBox();
+            this.videoFeed = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.videoFeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetect.Location = new System.Drawing.Point(1147, 118);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(121, 59);
+            this.btnDetect.TabIndex = 0;
+            this.btnDetect.Text = "Start";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.BtnDetect_Click);
+            // 
+            // camDevice
+            // 
+            this.camDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.camDevice.FormattingEnabled = true;
+            this.camDevice.Location = new System.Drawing.Point(1147, 47);
+            this.camDevice.Name = "camDevice";
+            this.camDevice.Size = new System.Drawing.Size(121, 24);
+            this.camDevice.TabIndex = 1;
+            // 
+            // videoFeed
+            // 
+            this.videoFeed.Location = new System.Drawing.Point(12, 21);
+            this.videoFeed.Name = "videoFeed";
+            this.videoFeed.Size = new System.Drawing.Size(1117, 606);
+            this.videoFeed.TabIndex = 2;
+            this.videoFeed.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1144, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Device:";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(1150, 252);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 22);
+            this.txtX.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1147, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1147, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Y";
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(1150, 337);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(100, 22);
+            this.txtY.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1337, 627);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.videoFeed);
+            this.Controls.Add(this.camDevice);
+            this.Controls.Add(this.btnDetect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.videoFeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.ComboBox camDevice;
+        private System.Windows.Forms.PictureBox videoFeed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ EMGU_FACE_DETECTION/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace EMGU_FACE_DETECTION
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection filter;
+        VideoCaptureDevice device;
+        int xval = 0;
+        int yval=0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filter)
+            {
+                camDevice.Items.Add(device.Name);
+            }
+            camDevice.SelectedIndex = 0;
+            device = new VideoCaptureDevice();
+
+        }
+
+        private void BtnDetect_Click(object sender, EventArgs e)
+        {
+            device = new VideoCaptureDevice(filter[camDevice.SelectedIndex].MonikerString);
+            device.NewFrame += Device_NewFrame;
+            device.Start();
+        }
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_default.xml");
+        private void Device_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            Image<Bgr, byte> greyImage = new Image<Bgr, byte>(bitmap);
+            Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(greyImage,1.2,5);
+            if (rectangles.Length > 0)
+            {
+                xval = rectangles[0].X + rectangles[0].Width / 2;
+                yval = rectangles[0].Y + rectangles[0].Height / 2;
+               
+            }
+            foreach (Rectangle rectangle in rectangles)
+            {
+                using(Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using(Pen pen=new Pen(Color.Blue, 5))
+                    {
+                        graphics.DrawRectangle(pen, rectangle);
+                    }
+                }
+            }
+            videoFeed.Image = bitmap;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (device.IsRunning)
+                device.Stop();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            txtX.Text = xval.ToString();
+            txtY.Text = yval.ToString();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (device.IsRunning)
+                device.Stop();
+        }
+    }
+}
